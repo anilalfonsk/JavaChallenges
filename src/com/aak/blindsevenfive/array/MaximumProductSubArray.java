8af054810ddb83aa784305fdf3bce93b50b424d1
anik2 <anik2@cisco.com>
2021-05-06 09:15:07 +0530

    Adding few more

---
+++ src/com/aak/blindsevenfive/array/MaximumProductSubArray.java
@@ -0,0 +1,27 @@
+package com.aak.blindsevenfive.array;
+
+public class MaximumProductSubArray {
+    public static void main(String[] args) {
+        MaximumProductSubArray maximumProductSubArray = new MaximumProductSubArray();
+        System.out.println(maximumProductSubArray.maxProduct(new int[]{-2,0,-1}));
+    }
+
+    public int maxProduct(int[] nums) {
+        int maxSum = nums[0];
+        int currentMax = nums[0];
+        int currentMin = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] < 0){
+                int tmp = currentMax;
+                currentMax = currentMin;
+                currentMin = tmp;
+            }
+
+            currentMax = Math.max(nums[i], currentMax * nums[i]);
+            currentMin = Math.min(nums[i], currentMin * nums[i]);
+            maxSum = Math.max(maxSum, currentMax);
+        }
+        return maxSum;
+    }
+}
